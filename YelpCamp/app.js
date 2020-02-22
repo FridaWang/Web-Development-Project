6a32e0b8f13f8ede2de502ed58d03a54449aab66
FridaWang <379788704@qq.com>
2020-02-22 23:53:57 +0800

    Complete YelpCamp v1,v2

---
+++ YelpCamp/app.js
@@ -0,0 +1,106 @@
+var express 	= require("express"),
+	app 		= express(),
+	bodyParser 	= require("body-parser"),
+	mongoose 	= require("mongoose");
+
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+
+/*
+Campground.create(
+	{
+		name:"Granite Hill",
+		image:"https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c7d2d79dc904fc45e_340.jpg",
+		description: "This is a huge granite hill, no bathrooms. No water. Beautiful granite!"
+	}, function(err, campground){
+		if(err){
+			console.log(err);
+		} else {
+			console.log("NEWLY CREATED CAMPGROUND: ");
+			console.log(campground);
+		}
+	});
+*/
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+/*
+var campgrounds = [
+		{name: "Salmon Creek", image:"https://pixabay.com/get/52e8d4444255ae14f6da8c7dda793f7f1636dfe2564c704c7d2d79dc904fc45e_340.jpg"},
+		{name: "Granite Hill", image:"https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c7d2d79dc904fc45e_340.jpg"},
+		{name: "Mountain Goat's Rest", image:"https://pixabay.com/get/57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c7d2d79dc904fc45e_340.jpg"},
+		{name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
+	];
+*/
+
+
+app.get("/", function(req, res){
+	res.render("landing");
+});
+
+//INDEX - 
+app.get("/campgrounds", function(req, res){
+	//Get all campgrouds from DB
+	Campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("index",{campgrounds: allCampgrounds});
+		}
+	});	
+});
+
+//CREATE
+app.post("/campgrounds", function(req, res){
+	// get data from form and add to campgrounds array
+	var name = req.body.name;
+	var image = req.body.image;
+	var description = req.body.description;
+	var newCampground = {name: name, image: image, description: description};
+	//Create a new campground and save to DB
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		} else {
+			//redirect back to campgrounds page
+			res.redirect("/campgrounds")
+		}
+	});
+});
+
+
+//NEW
+app.get("/campgrounds/new", function(req, res){
+	res.render("new");
+});
+
+//SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+	//find the campground with provided ID
+	Campground.findById(req.params.id, function(err, foundCampground){
+		if(err){
+			console.log(err);
+		} else {
+			//render show template with that campground
+			res.render("show", {campground: foundCampground});
+		}
+	});
+	
+})
+
+app.listen(3000, function(){
+	console.log("The YelpCamp Server Has Started!")
+});
